d0763dc9f630f3cd1b661297decfdd1e8a05e6ac
John Schwartz Jacobsen <john@johnschwartz.dk>
2018-08-21 19:46:08 +0200

    included path module part 2

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser');
 const jsonfile = require('jsonfile');
 
 const path = require('path');
-const jsonfileName = path.resolve('ugeplan.json');
+const jsonfileName = path.resolve(__dirname, 'ugeplan.json');
 
-app.set('views', './views');
+app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, './public')));
 app.use('/bower_components', express.static('bower_components'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
